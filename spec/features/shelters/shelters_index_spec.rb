43cee4337df98316d9c24150a70decab5896e63b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-28 17:42:32 -0700

    Finish test for user story 4

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -48,7 +48,7 @@ end
 # User story 4
 RSpec.describe "shelters index page", type: :feature do
   context 'as a visitor' do
-    it "can click New Shelter link and taken to a form" do
+    it "can click New Shelter, create new shelter, and sent back to shelters index" do
       shelter_1 = Shelter.create(name: "Mike's Shelter",
                                  address: '1331 17th Street',
                                  city: 'Denver',
@@ -63,8 +63,17 @@ RSpec.describe "shelters index page", type: :feature do
       visit "/shelters"
       click_link "New Shelter"
 
-      expect(page).to have_content("New Shelter info:")
-      expect(page).to have_content("Shelter name:")
+      expect(page).to have_content("New Shelter info")
+
+      fill_in 'shelter[name]', with: "Pets4Paws"
+      fill_in 'shelter[address]', with: "1234 Petco Street"
+      fill_in 'shelter[city]', with: "Broomfield"
+      fill_in 'shelter[state]', with: "CO"
+      fill_in 'shelter[zip]', with: "80020"
+
+      click_button "Create Shelter"
+
+      expect(page).to have_content("Pets4Paws")
     end
   end
 end
