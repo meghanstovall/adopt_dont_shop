d6eb41ae2fd16886939ba68ac5819992f66472e7
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-29 21:49:49 -0700

    Create models testing files

---
+++ spec/models/pet_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Pet, type: :model do
+  # describe "validations" do
+  #   it {should validate_presence_of :name}
+  # end
+
+  # describe "relationships" do
+  #   it {should have_many :pets}
+  # end
+
+  # describe "methods" do
+  #   it
+  # end
+end
