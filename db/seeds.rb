aef59be9e7258ed2afa526d7e7b06ce5eb82e551
Meghan Stovall <meghanstovall15@augustana.edu>
2020-01-30 17:37:23 -0700

    Fix pet in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ mikes = Shelter.create(name: "Mike's Shelter", address: '1331 17th Street', city
 megs = Shelter.create(name: "Meg's Shelter", address: '150 Main Street', city: 'Hershey', state: 'PA', zip: '17033')
 
 athena = Pet.create(image: "https://image.shutterstock.com/image-photo/funny-studio-portrait-smilling-puppy-600w-1008869716.jpg", name: "Athena", age: "1", sex: "Female", description: "butthead", status: "adoptable", shelter_id: megs.id)
-odell = Pet.create(image: "https://image.shutterstock.com/image-photo/dog-600w-587562362.jpg", name: "Odell", age: "4", sex: "Male", description: "good dog", status: "adoptable", shelter_id: mikes.id)
+odell = Pet.create(image: "https://image.shutterstock.com/image-photo/dog-600w-587562362.jpg", name: "Odell", age: "4", sex: "Male", description: "good dog", status: "pending", shelter_id: mikes.id)
