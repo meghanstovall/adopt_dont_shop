af379259ff4eb99e4fe460fdccee8359e3272917
Meghan Stovall <meghanstovall15@augustana.edu>
2020-02-02 13:20:07 -0700

    Add testing for welcome page

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,4 @@
 class Pet < ApplicationRecord
-  validates_presence_of :image, :name, :age, :sex, :description, :status
+  validates_presence_of :image, :name, :age, :sex, :description, :status, :shelter_id
   belongs_to :shelter
 end
--- spec/features/welcome/welcome_index_spec.rb
+++ spec/features/welcome/welcome_index_spec.rb
@@ -1,13 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.describe "welcome index page", type: :features do
-#   context 'as a visitor' do
-#     it 'can see welcome page' do
-#
-#       visit '/'
-#
-#       click_link "Shelters Idex"
-#       expect(current_path).to eq("/shelters/index")
-#     end
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "welcome index page", type: :features do
+  context 'as a visitor' do
+    it 'can see welcome page' do
+
+      visit '/'
+
+      has_link?("Shelters Index")
+      has_link?("Pets Index")
+      expect(page).to have_content("Adopt dont Shop!!")
+      expect(page).to have_content("Why you should adpot...")
+      expect(page).to have_content("Why you shouldn't buy...")
+    end
+  end
+end
